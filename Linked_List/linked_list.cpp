62bb7e673e99a064d1ebb2a39a42b51c2beee225
Ashish Shevale <shevaleashish@gmail.com>
2021-02-14 19:20:17 +0530

    Linked List:Print List

--- Linked_List/linked_list.cpp
+++ Linked_List/linked_list.cpp
@@ -17,6 +17,28 @@ typedef struct node
 node* HEAD;
 
 /*
+Function: print_list
+Args: head -> head pointer of list to be printed
+Desc: Print the linked list from the given element
+Returns: None
+*/
+void print_list(node* head)
+{
+    // create temporary node pointer that points to the head node
+    node* temp = head;
+
+    // repeat for all elements
+    while(temp != NULL)
+    {
+        // print the current element
+        cout << "\t" << temp->data;
+        // go to the next node
+        temp = temp->next;
+    }
+    cout << "\n\n";
+}
+
+/*
 Function: cleanup
 Args: head -> pointer to head of list to cleanup
 Desc: Deallocates space of all nodes in linked list
