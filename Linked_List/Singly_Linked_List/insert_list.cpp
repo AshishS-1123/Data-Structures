f476253b41bc769f4a46730176f0213a15abef4e
Ashish Shevale <shevaleashish@gmail.com>
2021-02-16 11:34:06 +0530

    Linked List:Insert At Head

---
+++ Linked_List/Singly_Linked_List/insert_list.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+Function: insert_at_head
+Args: head -> head pointer to list where element is to be inserted
+      new_node -> node to be inserted at head
+Desc: Inserts the given node before head of list
+Returns: head -> head pointer to new list
+*/
+node* insert_at_head(node* head, node* new_node)
+{
+    // make the new node point to head node
+    new_node->next = head;
+    // reassign head to the new node
+    head = new_node;
+
+    return head;
+}
+
+/*
+Function: insert_element
+Args: head -> head pointer of list where element is to be inserted
+      element -> item to be inserted
+      index -> position where element is to be inserted
+Desc: Insert the given item into the list at specified position
+Returns: head -> head pointer to new list
+*/
+node* insert_element(node* head, int element, int index)
+{
+    // create a new node to insert to linked list
+    node* new_node;
+    // alloc memory to the list
+    new_node = (node*) malloc( sizeof(node*) );
+    // add the element to the node
+    new_node->data = element;
+
+    // check we are inserting to an empty list
+    if(head == NULL)
+        // in this case, we can return the new node directly as head node
+        return new_node;
+
+    // if we need to insert before head node
+    if(index == 0)
+    {
+        // insert the new node before the head node
+        return insert_at_head(head, new_node);
+    }
+    else if(index < 0)
+    {
+        // in case index provided is -1,
+        // we insert the new node at the end of the list
+        return insert_at_end(head, new_node);
+    }
+    
+    // otherwise, insert the new node into the list
+    return insert_anywhere(head, new_node, index);
+}
+
