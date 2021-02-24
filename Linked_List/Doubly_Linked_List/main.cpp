05ac08a9c5979c5d3271b8df1a74e4106a7dcdd0
Ashish Shevale <shevaleashish@gmail.com>
2021-02-24 21:21:58 +0530

    Doubly Linked List:Create Main Function

---
+++ Linked_List/Doubly_Linked_List/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include "doubly_linked_list.cpp"
+#include "insert_list.cpp"
+#include "delete_list.cpp"
+#include "reverse_list.cpp"
+
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
+
