d6252af81aca3abeaab946c2b955ea8bfa2ac727
Ashish Shevale <shevaleashish@gmail.com>
2021-02-27 19:21:51 +0530

    Circular Linked List:Delete Element Utility

--- Linked_List/Circular_Linked_List/delete_list.cpp
+++ Linked_List/Circular_Linked_List/delete_list.cpp
@@ -3,6 +3,24 @@
 using namespace std;
 
 /*
+Function: delete_node_after
+Desc: deletes the node after the given node
+Args: node_before -> nod whose successor is to be deleted
+Returns: None
+*/
+void delete_node_after(node* node_before)
+{
+    // temporary node to store address of node to delete
+    node* node_to_delete = node_before->next;
+
+    // point this node to the next node of next node
+    node_before->next = node_before->next->next;
+
+    // deallocate space of node to be deleted
+    free(node_to_delete);
+}
+
+/*
 Function: delete_element
 Desc: deletes element at the given position
 Args: start -> node from where to count position
