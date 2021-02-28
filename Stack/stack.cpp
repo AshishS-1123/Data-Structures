a241e1fdc86f5eeeaacecef8242691b26322f270
Ashish Shevale <shevaleashish@gmail.com>
2021-02-28 21:47:26 +0530

    Stack:Overflow And Underflow Check

--- Stack/stack.cpp
+++ Stack/stack.cpp
@@ -27,5 +27,47 @@ class Stack
             cout << "\tInitialize Top to -1\n";
             cout << "\tInitialize Data Vector to capacity of " << capacity << "\n\n";
         }
+        
+        /*
+        Function: overflow
+        Desc: Checks if the stack has overflowed beyond its capacity
+        Args: None
+        Returns: overflow_occured -> bool values whether overflow occured
+        */
+        bool overflow()
+        {
+            // if stack is upto its capacity
+            if(top_pointer == stack_data.size())
+            {
+                // set size of top pointer when overflow
+                top_pointer = stack_data.size();
+
+                // return overflow occured
+                return true;
+            }
+            // otherwise, return not occured
+            return false;
+        }
+
+        /*
+        Function: underflow
+        Desc: Checks if the stack is underflowing
+        Args: None
+        Returns: underflow_occured -> bool value whether underflow occured
+        */
+        bool underflow()
+        {
+            // check if stack is underflowing
+            if(top_pointer < 0)
+            {
+                // set value of top pointer when empty
+                top_pointer = -1;
+
+                // returnunderflow occured
+                return true;
+            }
+            // otherwise, return not occured
+            return false;
+        }
 
 };
