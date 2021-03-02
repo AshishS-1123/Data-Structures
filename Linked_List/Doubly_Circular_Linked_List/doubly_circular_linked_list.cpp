132fe152d57197c7038cc80d611a214936935388
Ashish Shevale <shevaleashish@gmail.com>
2021-03-02 21:19:48 +0530

    Doubly Circular List:CleanUp

--- Linked_List/Doubly_Circular_Linked_List/doubly_circular_linked_list.cpp
+++ Linked_List/Doubly_Circular_Linked_List/doubly_circular_linked_list.cpp
@@ -13,3 +13,33 @@ typedef struct node
     struct node* prev;
 }node;
 
+// a node to point to some element in the list
+node* start;
+
+/*
+Function: cleanup
+Desc: deallocates memory of all nodes in list
+Args: start -> pointer to any node in list
+Returns: None
+*/
+void cleanup(node* start)
+{
+    // pointer for traversal
+    node* end = start;
+
+    // loop through all the nodes
+    do
+    {
+        cout << "\tDelete " << end->data << "\n";
+        // temporarily hold location of end node
+        node* temp = end;
+        
+        // increment the end node
+        end = end->next;
+        
+        // deallocate the current node
+        free(temp);
+
+    }while(end != start);
+
+}
