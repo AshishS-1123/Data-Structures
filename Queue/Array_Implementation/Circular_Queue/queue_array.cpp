23e7f0a76ff470028e9ab9aa63ec95d19b24ce4d
Ashish Shevale <shevaleashish@gmail.com>
2021-03-20 22:46:05 +0530

    Circular Queue Array: Print Queue

--- Queue/Array_Implementation/Circular_Queue/queue_array.cpp
+++ Queue/Array_Implementation/Circular_Queue/queue_array.cpp
@@ -38,3 +38,36 @@ Queue create_queue()
     return q;
 }
 
+/*
+Function: print_queue
+Desc: prints all elements of queue from front to rear
+Args: q -> Queue object to be printed
+Returns: None
+*/
+void print_queue(Queue q)
+{
+    // check if queue is empty
+    if(q.front == -1 && q.rear == -1)
+    {
+        cout << "\tQueue is Empty... Underflow\n\n";
+        return;
+    }
+    
+    cout << "\t(front) "; 
+
+    // repeat for all elements in the queue
+    //for(int i = q.front; i != (q.rear+1)%CAPACITY; )
+    int i = q.front;
+    do
+    {
+        // print the element
+        cout << q.data[i] << " ";
+
+        // increment i
+        i = (i + 1) % CAPACITY;
+
+    }while(i != (q.rear+1)%CAPACITY);
+
+    cout << "(rear)\n\n";
+}
+
