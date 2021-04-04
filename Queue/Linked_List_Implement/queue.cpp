346153653c36aacf981fd519b7ee5bc833b9ffb2
Ashish Shevale <shevaleashish@gmail.com>
2021-04-04 18:15:59 +0530

    Queue Linked List: Create Queue

--- Queue/Linked_List_Implement/queue.cpp
+++ Queue/Linked_List_Implement/queue.cpp
@@ -14,3 +14,22 @@ typedef struct Queue
     node* rear;
 } Queue;
 
+/*
+Function: create_queue
+Desc: creates a new queue object
+Args: None
+Returns: q -> queue object created
+*/
+Queue create_queue()
+{
+    // create a queue object
+    Queue q;
+
+    // initialize the front and rear pointers
+    q.front = NULL;
+    q.rear = NULL;
+
+    // return the created queue
+    return q;
+}
+
