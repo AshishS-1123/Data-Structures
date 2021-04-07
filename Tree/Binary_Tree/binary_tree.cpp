a2a34d8bd0c13ac74bc79ba7d997ce8d86f15b76
Ashish Shevale <shevaleashish@gmail.com>
2021-04-07 22:54:17 +0530

    Binary Tree: Define Class

---
+++ Tree/Binary_Tree/binary_tree.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+/*
+Class: binaryTree
+Desc: class for representing a binary tree.
+      Representation is done using a set of 3 arrays-
+        1. data -> array to hold the data of a node
+        2. leftChild -> array to hold index of left child
+        3. rightChild -> array to hold index of right child
+Attr: data -> array to hold data of node
+      leftChild -> array to hold index of left child
+      rightChild -> array to hold index of right child
+*/
+class binaryTree
+{
+    
+};
+
