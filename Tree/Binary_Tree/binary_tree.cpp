eaa86dd7affb507b6b4efffcb93948a1204f3d50
Ashish Shevale <shevaleashish@gmail.com>
2021-04-07 22:55:00 +0530

    Binary Tree: Utility Functions

--- Tree/Binary_Tree/binary_tree.cpp
+++ Tree/Binary_Tree/binary_tree.cpp
@@ -17,6 +17,68 @@ Attr: data -> array to hold data of node
 */
 class binaryTree
 {
-    
+    //private:
+    public:
+        vector<int> data;
+        vector<int> leftChild;
+        vector<int> rightChild;
+    public:
+        /*
+        Function: binaryTree
+        Desc: constructor for Binary Tree class
+        Args: None
+        Returns: None
+        */
+        binaryTree()
+        {
+
+        }
+
+        /*
+        Function: left_child
+        Desc: return left child of given node
+        Args: idx -> index of element whose left child to return
+        Returns: left -> index of left child of given element
+        */
+
+        int left_child(int idx)
+        {
+            return leftChild[idx];
+        }
+
+        /*
+        Function: right_child
+        Desc: return right child of given node
+        Args: idx -> index of element whose right child to return
+        Returns: right -> index of right child of given element
+        */
+        int right_child(int idx)
+        {
+            return rightChild[idx];
+        }
+
+        /*
+        Function: empty
+        Desc: checks if the tree is empty
+        Args: None
+        Returns: bool -> true of tree is empty, false otherwise
+        */
+        bool empty()
+        {
+            return (data.size() == 0);
+        }
+
+        /*
+        Function: get_data
+        Desc: returns data present in given node
+        Args: idx -> index whose data to return
+        Returns: data -> data present at given index
+        */
+        int get_data(int idx)
+        {
+            return data[idx];
+        }
+
+
 };
 
