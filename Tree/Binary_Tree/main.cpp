c155dcee51ae0046bc5c0c81968f955af1c67989
Ashish Shevale <shevaleashish@gmail.com>
2021-04-09 23:04:54 +0530

    Tree: Test Insert

--- Tree/Binary_Tree/main.cpp
+++ Tree/Binary_Tree/main.cpp
@@ -10,7 +10,34 @@ int main()
     // create a tree object
     binaryTree tree = binaryTree();
 
-        return 0;
+    // insert 10, 20, 30, 40, 50, 60
+
+    cout << "\nAfter adding 10...";
+    tree.insert_element(10);
+    tree.print_tree();
+
+    cout << "\nAfter adding 20...";
+    tree.insert_element(20);
+    tree.print_tree();
+
+    cout << "\nAfter adding 30...";
+    tree.insert_element(30);
+    tree.print_tree();
+
+    cout << "\nAfter adding 40...";
+    tree.insert_element(40);
+    tree.print_tree();
+
+    cout << "\nAfter adding 50...";   
+    tree.insert_element(50);
+    tree.print_tree();
+
+    cout << "\nAfter adding 60...";
+    tree.insert_element(60);
+    tree.print_tree();
+
+    
+    return 0;
 }
 
 
